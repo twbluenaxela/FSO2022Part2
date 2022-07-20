4f739fc964e138701e831788739926c0c58c693c
阿洋 <rednaxela64@outlook.com>
2022-07-20 20:20:05 +0000

    Seperate all modules

--- course/src/components/Content.js
+++ course/src/components/Content.js
@@ -7,12 +7,6 @@ const Content = ({ parts }) => {
         {parts.map((part) => {
           return <Part key={part.id} content={part.name} amount={part.exercises}></Part>;
         })}
-        {/**
-         * <Part content={props.parts[0].name} amount={props.parts[0].exercises} />
-        <Part content={props.parts[1].name} amount={props.parts[1].exercises} />
-        <Part content={props.parts[2].name} amount={props.parts[2].exercises} />
-         * 
-         */}
       </div>
     );
   };
