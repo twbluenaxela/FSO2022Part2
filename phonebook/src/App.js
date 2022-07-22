9da66feaa32eb24b2771a896cd97f3eedeb58a9b
阿洋 <rednaxela64@outlook.com>
2022-07-22 23:45:31 +0000

    Add database to phonebook

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from 'axios'
 
 const Persons = ({ persons, filter }) => {
 
@@ -93,15 +94,21 @@ const PersonForm = ({
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
+
+  const gitpodBackendUrl = "https://3001-twbluenaxel-fso2022part-q6p1ytmwo86.ws-us54.gitpod.io"
+
+  useEffect(()=>{
+    console.log("loading...")
+    axios.get(gitpodBackendUrl + "/persons")
+    .then(response => {
+      console.log('promise fulfilled', response.data)
+      setPersons(response.data)
+    })
+  },[])
 
   return (
     <div>
