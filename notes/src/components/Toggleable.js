2272bb0024c111df5a59c6d10712c381c14ecc69
Nick Yu <rednaxela64@outlook.com>
2022-08-19 22:34:47 -0700

    check if anything stragglers

--- notes/src/components/Toggleable.js
+++ notes/src/components/Toggleable.js
@@ -3,37 +3,37 @@ import PropTypes from 'prop-types'
 
 
 const Toggleable = forwardRef ((props, ref) => {
-    const [visible, setVisible] = useState(false)
-    const hideWhenVisible = { display: visible ? 'none' : '' }
-    const showWhenVisible = { display: visible ? '' : 'none' }
+  const [visible, setVisible] = useState(false)
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
 
-    const toggleVisibility = () => {
-        setVisible(!visible)
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  useImperativeHandle(ref, () => {
+    return {
+      toggleVisibility
     }
+  })
 
-    useImperativeHandle(ref, () => {
-      return {
-        toggleVisibility
-      }
-    })
+  Toggleable.propTypes = {
+    buttonLabel: PropTypes.string.isRequired
+  }
 
-    Toggleable.propTypes = {
-      buttonLabel: PropTypes.string.isRequired
-    }
+  Toggleable.displayName = 'Toggleable'
 
-    Toggleable.displayName = 'Toggleable'
-
-    return (
-        <div>
-        <div style={hideWhenVisible}>
-          <button onClick={toggleVisibility}>{props.buttonLabel}</button>
-        </div>
-        <div style={showWhenVisible}>
-            {props.children}
-            <button onClick={toggleVisibility} >cancel</button>
-        </div>
+  return (
+    <div>
+      <div style={hideWhenVisible}>
+        <button onClick={toggleVisibility}>{props.buttonLabel}</button>
       </div>
-    )
+      <div style={showWhenVisible}>
+        {props.children}
+        <button onClick={toggleVisibility} >cancel</button>
+      </div>
+    </div>
+  )
 
 })
 
