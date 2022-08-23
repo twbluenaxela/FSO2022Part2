6b06b3e68d1e073dcec7d733adc4377400945097
Nick Yu <rednaxela64@outlook.com>
2022-08-23 15:39:17 -0700

    fix login page issues with cypress test

--- notes/cypress/e2e/note_app.spec.cy.js
+++ notes/cypress/e2e/note_app.spec.cy.js
@@ -1,39 +1,61 @@
 describe('Note app', () => {
-    beforeEach(function () {
-        cy.visit('http://localhost:3000')
-    })
-    it(`front page can be opened`, () => {
-        cy.contains('Notes')
-        cy.contains('Note app, Department of Computer Science, University of Helsinki 2022')
-    })
+  beforeEach(function () {
+    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      name: 'Tudou',
+      username: 'tudou',
+      password: 'tudou'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user)
+    cy.visit('http://localhost:3000')
+  })
+  it('front page can be opened', () => {
+    cy.contains('Notes')
+    cy.contains('Note app, Department of Computer Science, University of Helsinki 2022')
+  })
 
-    // it('front page contains random text', function() {
-    //     cy.visit('http://localhost:3000')
-    //     cy.contains('wtf is this app?')
-    // })
-    // it('login form can be opened', function() {
-    //     cy.contains('login').click()
-    // })
-    it('user can login', function() {
-        cy.contains('login').click()
-        cy.get('#username').type('admin')
-        cy.get('#password').type('salainen')
-        cy.get('#login-button').click()
+  // it('front page contains random text', function() {
+  //     cy.visit('http://localhost:3000')
+  //     cy.contains('wtf is this app?')
+  // })
+  // it('login form can be opened', function() {
+  //     cy.contains('login').click()
+  // })
+  it('user can login', function() {
+    cy.contains('login').click()
+    cy.get('#username').type('tudou')
+    cy.get('#password').type('tudou')
+    cy.get('#login-button').click()
 
-        cy.contains('Nick logged in')
+    cy.contains('Tudou logged in')
+  })
+  describe('when logged in', function () {
+    beforeEach(function() {
+      cy.contains('login').click()
+      cy.get('input:first').type('tudou')
+      cy.get('input:last').type('tudou')
+      cy.get('#login-button').click()
     })
-    describe('when logged in', function () {
-        beforeEach(function() {
-            cy.contains('login').click()
-            cy.get('input:first').type('admin')
-            cy.get('input:last').type('salainen')
-            cy.get('#login-button').click()    
-        })
-        it('a new note can be created', function () {
-            cy.contains('new note').click()
-            cy.get('input').type('a note created by cypress')
-            cy.contains('save').click()
-            cy.contains('a note created by cypress')
-        })
+    it('a new note can be created', function () {
+      cy.contains('new note').click()
+      cy.get('input').type('a note created by cypress')
+      cy.contains('save').click()
+      cy.contains('a note created by cypress')
     })
+    describe('and a note exists', function () {
+      beforeEach(function() {
+        cy.contains('new note').click()
+        cy.get('input').type('another note cypress')
+        cy.contains('save').click()
+      })
+      it('it can be made important', function() {
+        cy.contains('another note cypress')
+          .contains('make important')
+          .click()
+
+        cy.contains('another note cypress')
+          .contains('make not important')
+      })
+    })
+  })
 })
