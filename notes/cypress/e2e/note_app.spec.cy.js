540682060dea90bdb8f8a716be4c2e81bd25664e
Nick Yu <rednaxela64@outlook.com>
2022-08-24 14:24:53 -0700

    add commands for logging in and making a new note

--- notes/cypress/e2e/note_app.spec.cy.js
+++ notes/cypress/e2e/note_app.spec.cy.js
@@ -13,22 +13,14 @@ describe('Note app', () => {
     cy.contains('Notes')
     cy.contains('Note app, Department of Computer Science, University of Helsinki 2022')
   })
-
-  it.only('login fails with wrong password', function () {
+  it('login form can be opened', function() {
     cy.contains('login').click()
-    cy.get('#username').type('mluukai')
-    cy.get('#password').type('wrong')
+    cy.get('#username').type('tudou')
+    cy.get('#password').type('tudou')
     cy.get('#login-button').click()
 
-    cy.contains('wrong credentials')
-  })
-  // it('front page contains random text', function() {
-  //     cy.visit('http://localhost:3000')
-  //     cy.contains('wtf is this app?')
-  // })
-  // it('login form can be opened', function() {
-  //     cy.contains('login').click()
-  // })
+    cy.contains('Tudou logged in')
+})
   it('user can login', function() {
     cy.contains('login').click()
     cy.get('#username').type('tudou')
@@ -37,24 +29,51 @@ describe('Note app', () => {
 
     cy.contains('Tudou logged in')
   })
+
+
+  // it('login fails with wrong password', function () {
+  //   cy.contains('login').click()
+  //   cy.get('#username').type('mluukai')
+  //   cy.get('#password').type('wrong')
+  //   cy.get('#login-button').click()
+  //   cy.get('.error')
+  //   .should('contain', 'wrong credentials')
+  //   .and('have.css', 'color', 'rgb(255, 0, 0)')
+  //   .and('have.css', 'border-style', 'solid')
+
+  //   cy.get('html').should('not.contain', 'Matti Luukkainen logged in')
+  //   //wow... so remember to include the space between each color value or else the test will fail
+  // })
+  // it('front page contains random text', function() {
+  //     cy.visit('http://localhost:3000')
+  //     cy.contains('wtf is this app?')
+  // })
+
+  
   describe('when logged in', function () {
     beforeEach(function() {
-      cy.contains('login').click()
-      cy.get('input:first').type('tudou')
-      cy.get('input:last').type('tudou')
-      cy.get('#login-button').click()
+      cy.login({ username: 'tudou', password: 'tudou' })
+      // cy.contains('login').click()
+      // cy.get('input:first').type('tudou')
+      // cy.get('input:last').type('tudou')
+      // cy.get('#login-button').click()
     })
     it('a new note can be created', function () {
       cy.contains('new note').click()
       cy.get('input').type('a note created by cypress')
       cy.contains('save').click()
+
       cy.contains('a note created by cypress')
     })
     describe('and a note exists', function () {
       beforeEach(function() {
-        cy.contains('new note').click()
-        cy.get('input').type('another note cypress')
-        cy.contains('save').click()
+        // cy.contains('new note').click()
+        // cy.get('input').type('another note cypress')
+        // cy.contains('save').click()
+        cy.createNote({
+          content: 'another note cypress',
+          important: false
+        })
       })
       it('it can be made important', function() {
         cy.contains('another note cypress')
--- notes/src/App.js
+++ notes/src/App.js
@@ -140,7 +140,7 @@ const App = () => {
       setUsername('')
       setPassword('')
     } catch (exception) {
-      setErrorMessage('Wrong credentials')
+      setErrorMessage('wrong credentials')
       setTimeout(() => {
         setErrorMessage(null)
       }, 5000)
